65154b0e25eba57dfd14cd320b6a896679f6ffb8
elinthim <elin_thim@hotmail.com>
2023-04-21 00:26:49 +0200

    Add project files.

---
+++ ASPNETMVCCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        private readonly MVCDemoDbContext mvcDemoDbcontext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+            };
+            mvcDemoDbContext.Employees.Add(employee);
+            mvcDemoDbContext.SaveChanges();
+            return RedirectToAction("Add");
+        }
+    }
+}
+
